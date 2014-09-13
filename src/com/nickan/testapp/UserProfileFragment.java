75604f4ab33975f66ca212e601f4b7fbd8488ae2
Nickan <colete_nico@yahoo.com>
2014-09-13 10:16:37 -0700

    Save point

--- src/com/nickan/testapp/UserProfileFragment.java
+++ src/com/nickan/testapp/UserProfileFragment.java
@@ -3,6 +3,8 @@ package com.nickan.testapp;
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 
 import android.os.Bundle;
@@ -21,26 +23,36 @@ public class UserProfileFragment extends Fragment {
 	TextView link;
 	TextView gender;
 	TextView locale;
-	TextView age_range;
 	
 	View view;
+	
+	
+	private UiLifecycleHelper uiHelper;
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		
+		@Override
+		public void call(Session session, SessionState state, Exception exception) {
+			onStateStatusChange(session, state, exception);
+		}
+	};
+	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
 		view = inflater.inflate(R.layout.user_profile, container, false);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+		uiHelper.onCreate(savedInstanceState);
 		return view;
 	}
 	
-	private void buildUserInfo() {
+	private void buildUserInfo(Session session) {
 		id = (TextView) view.findViewById(R.id.id);
 		name = (TextView) view.findViewById(R.id.name);
 		link = (TextView) view.findViewById(R.id.link);
 		gender = (TextView) view.findViewById(R.id.gender);
 		locale = (TextView) view.findViewById(R.id.locale);
-		age_range = (TextView) view.findViewById(R.id.age_range);
 		
-		Session session = Session.getActiveSession();
 		if (session.isOpened()) {
 			Log.e(TAG, "Session is open");
 			Request.newMeRequest(session, new Request.GraphUserCallback() {
@@ -72,10 +84,37 @@ public class UserProfileFragment extends Fragment {
 		}
 	}
 	
+	
+	private void onStateStatusChange(Session session, SessionState state, Exception exception) {
+		if (session != null && session.isOpened()) {
+			buildUserInfo(session);
+		}
+	}
+	
+	
 	@Override
 	public void onResume() {
 		super.onResume();
-		buildUserInfo();
+		uiHelper.onResume();
  	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		uiHelper.onPause();
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle state) {
+		super.onSaveInstanceState(state);
+		uiHelper.onSaveInstanceState(state);
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+	
 	
 }
